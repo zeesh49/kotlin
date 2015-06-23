df4151a76861d58ca9f7cee074241de62a4aabc2
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-06-23 15:51:58 +0300

    ~JS fixes after review: assert is/as is expanded

--- js/js.translator/testData/expression/cast/_commonFiles/asserts.kt
+++ js/js.translator/testData/expression/cast/_commonFiles/asserts.kt
@@ -1,5 +1,9 @@
 package kotlin
 
+// CHECK_NOT_REFERENCED: Kotlin.isInstanceOf
+// CHECK_NOT_REFERENCED: Kotlin.isTypeOf
+// CHECK_NOT_REFERENCED: Kotlin.orNull
+
 fun failsClassCast(message: String, fn: ()->Unit) {
     try {
         fn()
