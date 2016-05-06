9e5a4c946191913676fc379c08abe23f954f6f9e
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-05-06 20:39:49 +0300

    ~ fix tests on windows

--- compiler/tests/org/jetbrains/kotlin/daemon/CompilerDaemonTest.kt
+++ compiler/tests/org/jetbrains/kotlin/daemon/CompilerDaemonTest.kt
@@ -116,6 +116,7 @@ class CompilerDaemonTest : KotlinIntegrationTestBase() {
 
     fun testCodeWithNonAsciiSymbols() {
         fun pathToTestFile(extension: String) = getTestDataPathBase() + "/launcher/withNonAsciiSymbols.$extension"
+        fun String.normalize() = replace('/', File.separatorChar)
 
         withFlagFile(getTestName(true), ".alive") { flagFile ->
             val daemonOptions = DaemonOptions(runFilesPath = File(tmpdir, getTestName(true)).absolutePath, verbose = true)
@@ -141,7 +142,7 @@ class CompilerDaemonTest : KotlinIntegrationTestBase() {
                 KotlinCompilerClient.shutdownCompileService(compilerId, daemonOptions)
                 daemonShotDown = true
 
-                assertEquals(File(pathToTestFile("out")).readText(), result.out)
+                assertEquals(File(pathToTestFile("out")).readText().normalize(), result.out)
             }
             finally {
                 if (!daemonShotDown) {
