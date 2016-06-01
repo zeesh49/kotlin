8960c94c3296b856aef5d45cf5e9feb92747940e
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-01 18:28:06 +0300

    KT-2752: add checkers for inconsistent usage of JsName

--- js/js.frontend/src/org/jetbrains/kotlin/js/resolve/JsPlatformConfigurator.kt
+++ js/js.frontend/src/org/jetbrains/kotlin/js/resolve/JsPlatformConfigurator.kt
@@ -19,9 +19,9 @@ package org.jetbrains.kotlin.js.resolve
 import org.jetbrains.kotlin.container.StorageComponentContainer
 import org.jetbrains.kotlin.container.useImpl
 import org.jetbrains.kotlin.container.useInstance
-import org.jetbrains.kotlin.js.naming.FQNGenerator
 import org.jetbrains.kotlin.js.resolve.diagnostics.JsCallChecker
 import org.jetbrains.kotlin.js.resolve.diagnostics.JsNameChecker
+import org.jetbrains.kotlin.js.resolve.diagnostics.JsNameClashChecker
 import org.jetbrains.kotlin.js.resolve.diagnostics.NativeInnerClassChecker
 import org.jetbrains.kotlin.resolve.IdentifierChecker
 import org.jetbrains.kotlin.resolve.OverloadFilter
@@ -32,7 +32,7 @@ import org.jetbrains.kotlin.types.DynamicTypesAllowed
 object JsPlatformConfigurator : PlatformConfigurator(
         DynamicTypesAllowed(),
         additionalDeclarationCheckers = listOf(NativeInvokeChecker(), NativeGetterChecker(), NativeSetterChecker(),
-                                               NativeInnerClassChecker()),
+                                               NativeInnerClassChecker(), JsNameChecker),
         additionalCallCheckers = listOf(),
         additionalTypeCheckers = listOf(),
         additionalSymbolUsageValidators = listOf(),
@@ -46,6 +46,6 @@ object JsPlatformConfigurator : PlatformConfigurator(
         container.useImpl<JsCallChecker>()
         container.useInstance(SyntheticScopes.Empty)
         container.useInstance(JsTypeSpecificityComparator)
-        container.useInstance(JsNameChecker())
+        container.useInstance(JsNameClashChecker())
     }
 }
