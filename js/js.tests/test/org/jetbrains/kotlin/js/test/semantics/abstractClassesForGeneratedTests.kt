ae628245320bd3895579e8eeeba1b8e27e761004
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-03 16:02:39 +0300

    KT-2752: add diagnostics that reports about conflicting overridden declarations

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/abstractClassesForGeneratedTests.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/abstractClassesForGeneratedTests.kt
@@ -16,14 +16,14 @@
 
 package org.jetbrains.kotlin.js.test.semantics
 
-import com.google.common.collect.Lists
-import org.jetbrains.kotlin.js.JavaScript
-import org.jetbrains.kotlin.js.config.EcmaVersion
 import org.jetbrains.kotlin.js.test.AbstractSingleFileTranslationWithDirectivesTest
 import org.jetbrains.kotlin.js.test.KotlinJSMultiFileTest
 import org.jetbrains.kotlin.js.test.MultipleModulesTranslationTest
 import org.jetbrains.kotlin.js.test.SingleFileTranslationTest
-import java.io.File
+import org.jetbrains.kotlin.js.test.AbstractSingleFileTranslationWithDirectivesTest
+import org.jetbrains.kotlin.js.test.KotlinJSMultiFileTest
+import org.jetbrains.kotlin.js.test.MultipleModulesTranslationTest
+import org.jetbrains.kotlin.js.test.SingleFileTranslationTest
 
 abstract class AbstractBlackBoxTest(d: String) : SingleFileTranslationTest(d) {
     override fun doTest(filename: String) = checkBlackBoxIsOkByPath(filename)
