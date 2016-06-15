21085522bcef1fbb95d306b9412c0ed5f4c9309d
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-15 18:07:28 +0300

    KT-2752: remove diagnostic that reports about overridden method having several names from different sources. Generate proxies instead. Fix reporting name clash when two distinct fake overrides require same name.

--- compiler/testData/codegen/box/bridges/jsName.kt
+++ compiler/testData/codegen/box/bridges/jsName.kt
@@ -1,12 +1,12 @@
 // TARGET_BACKEND: JS
 package foo
 
-@native interface A {
-    fun foo(value: Int): String
+interface A {
+    @JsName("foo") fun foo(value: Int): String
 }
 
 interface B {
-    fun foo(value: Int): String
+    @JsName("bar") fun foo(value: Int): String
 }
 
 class C : A, B {
@@ -28,7 +28,7 @@ fun box(): String {
 
     val d: dynamic = C()
     assertEquals("C.foo(42)", d.foo(42))
-    assertEquals("C.foo(99)", d.`foo_za3lpa$`(99))
+    assertEquals("C.foo(99)", d.bar(99))
 
     val da: A = E()
     assertEquals("D.foo(55)", da.foo(55))
@@ -38,7 +38,8 @@ fun box(): String {
 
     val dd: dynamic = E()
     assertEquals("D.foo(42)", dd.foo(42))
-    assertEquals("D.foo(99)", dd.`foo_za3lpa$`(99))
+    assertEquals("D.foo(99)", dd.bar(99))
+    assertEquals("D.foo(88)", dd.`foo_za3lpa$`(88))
 
     return "OK"
 }
