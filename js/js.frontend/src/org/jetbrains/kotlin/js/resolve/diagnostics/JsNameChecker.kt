63ff21745beb877d74d65c0175a8aca9c4ece6fb
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-16 16:08:59 +0300

    KT-2752: prohibit JSName on extension properties

--- js/js.frontend/src/org/jetbrains/kotlin/js/resolve/diagnostics/JsNameChecker.kt
+++ js/js.frontend/src/org/jetbrains/kotlin/js/resolve/diagnostics/JsNameChecker.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.js.translate.utils.AnnotationsUtils
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.DeclarationChecker
+import org.jetbrains.kotlin.resolve.descriptorUtil.isExtension
 
 object JsNameChecker : DeclarationChecker {
     override fun check(declaration: KtDeclaration, descriptor: DeclarationDescriptor, diagnosticHolder: DiagnosticSink,
@@ -50,6 +51,11 @@ object JsNameChecker : DeclarationChecker {
             is PropertyAccessorDescriptor -> {
                 if (AnnotationsUtils.getJsName(descriptor.correspondingProperty) != null) {
                     diagnosticHolder.report(ErrorsJs.JS_NAME_ON_ACCESSOR_AND_PROPERTY.on(jsNamePsi))
+                }
+            }
+            is PropertyDescriptor -> {
+                if (descriptor.isExtension) {
+                    diagnosticHolder.report(ErrorsJs.JS_NAME_PROHIBITED_FOR_EXTENSION_PROPERTY.on(jsNamePsi))
                 }
             }
         }
