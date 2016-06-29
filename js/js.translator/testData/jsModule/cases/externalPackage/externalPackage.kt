3ebb219b09c0160e893b78a965c68610ade7a83b
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-29 15:44:06 +0300

    KT-12877: modify tests to check whether mutable top-level properties of JsModule files are supported

--- js/js.translator/testData/jsModule/cases/externalPackage/externalPackage.kt
+++ js/js.translator/testData/jsModule/cases/externalPackage/externalPackage.kt
@@ -15,6 +15,8 @@ package foo
 
 @native val bar: Int = noImpl
 
+@native var mbar: Int = noImpl
+
 fun box(): String {
     val a = A(23)
     assertEquals(23, a.x)
@@ -25,6 +27,9 @@ fun box(): String {
 
     assertEquals(365, foo(42))
     assertEquals(423, bar)
+
+    mbar = 523
+    assertEquals(523, mbar)
 
     return "OK"
 }
