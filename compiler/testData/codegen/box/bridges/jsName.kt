c171620b615205be2fba7339e0e1093b101aa49b
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-08-23 14:17:10 +0300

    fixup 8f7c3e9397bf5ec2965d738a5e4b3367e2702d32

--- compiler/testData/codegen/box/bridges/jsName.kt
+++ compiler/testData/codegen/box/bridges/jsName.kt
@@ -9,8 +9,12 @@ interface B {
     @JsName("bar") fun foo(value: Int): String
 }
 
-class C : A, B {
+open class C : A, B {
     override fun foo(value: Int) = "C.foo($value)"
+}
+
+class CDerived : C() {
+    override fun foo(value: Int) = "CDerived.foo($value)"
 }
 
 open class D {
@@ -26,9 +30,19 @@ fun box(): String {
     val b: B = C()
     assertEquals("C.foo(23)", b.foo(23))
 
+    val a2: A = CDerived()
+    assertEquals("CDerived.foo(55)", a2.foo(55))
+
+    val b2: B = CDerived()
+    assertEquals("CDerived.foo(23)", b2.foo(23))
+
     val d: dynamic = C()
     assertEquals("C.foo(42)", d.foo(42))
     assertEquals("C.foo(99)", d.bar(99))
+
+    val d2: dynamic = CDerived()
+    assertEquals("CDerived.foo(42)", d2.foo(42))
+    assertEquals("CDerived.foo(99)", d2.bar(99))
 
     val da: A = E()
     assertEquals("D.foo(55)", da.foo(55))
