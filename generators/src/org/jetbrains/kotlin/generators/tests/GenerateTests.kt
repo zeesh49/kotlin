68a074bfab88b3138f646ee2ced1e4046e86adb4
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-08-26 14:24:02 +0300

    JS: move InlineMultiModuleTestGenerated under new test class

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1139,7 +1139,7 @@ fun main(args: Array<String>) {
         }
 
         testClass<AbstractInlineMultiModuleTest>() {
-            model("inlineMultiModule/cases", extension = null, recursive = false)
+            model("inlineMultiModule/cases")
         }
 
         testClass<AbstractLabelTest>() {
