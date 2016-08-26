03500d5e797fffc1c989cc98decc82b97412f760
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-08-26 16:03:09 +0300

    JS: refactor test classes

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1110,12 +1110,8 @@ fun main(args: Array<String>) {
             model("reservedWords/cases")
         }
 
-        testClass<AbstractFunctionCallableReferenceTest> {
-            model("callableReference/function/cases")
-        }
-
-        testClass<AbstractPropertyCallableReferenceTest> {
-            model("callableReference/property/cases")
+        testClass<AbstractCallableReferenceTest> {
+            model("callableReference/")
         }
 
         testClass<AbstractDynamicTest>() {
