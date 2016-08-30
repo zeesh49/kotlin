d3da523c8a965f2bdbb8cc3e432803ed17b426f5
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-08-30 15:31:53 +0300

    JS: move more test to box tests

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1109,10 +1109,6 @@ fun main(args: Array<String>) {
         testClass<AbstractBoxJsTest>() {
             model("box/", pattern = "^([^_](.+))\\.kt$")
         }
-
-        testClass<AbstractReservedWordTest>() {
-            model("reservedWords/cases")
-        }
     }
 
     testGroup("js/js.tests/test", "compiler/testData") {
